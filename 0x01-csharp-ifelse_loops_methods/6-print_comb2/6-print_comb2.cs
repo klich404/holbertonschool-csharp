b5342e1d6ee79ea58dce46dba84357aa05aab5d9
klich404 <klich4010@gmail.com>
2021-11-30 16:49:42 -0500

    task 6

--- 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -6,7 +6,18 @@ namespace _6_print_comb2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x, y;
+
+            for (x = 0; x < 10; x++)
+            {
+                for (y = x + 1; y < 10; y++)
+                {
+                    if (x == 8 && y == 9)
+                        Console.WriteLine("{0}{1}", x, y);
+                    else
+                        Console.Write("{0}{1}, ", x, y);
+                }
+            }
         }
     }
 }
