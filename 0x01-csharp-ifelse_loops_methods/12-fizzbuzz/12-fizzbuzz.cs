d651bff6d134fa3faf6e17333ef1872b6c46594f
klich404 <klich4010@gmail.com>
2021-11-30 16:34:39 -0500

    task 12

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 1; n <= 100; n++)
+            {
+                if (n % 3 == 0 && n % 5 == 0)
+                    Console.Write("FizzBuzz");
+                else if (n % 3 == 0)
+                    Console.Write("Fizz");
+                else if (n % 5 == 0)
+                    Console.Write("Buzz");
+                else
+                    Console.Write(n);
+                if (n != 100)
+                    Console.Write(" ");
+                else
+                    Console.WriteLine();
+            }
+        }
+    }
+}
