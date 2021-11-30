a2d9e92b7c4f3db19e0053e9595699970af4d95c
klich404 <klich4010@gmail.com>
2021-11-30 15:59:30 -0500

    Dtask 7 and 8

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        if (c >= 97 && c <= 122)
+            return (true);
+        return (false);
+    }
+}
---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        if (number < 0)
+            number = number * -1;
+        Console.Write(number % 10);
+        return (number % 10);
+    }
+}
