b59e3b3fe5c1c53a4e6315e69be5cea44016fb6c
klich404 <klich4010@gmail.com>
2021-11-30 12:30:52 -0500

    task 4

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            for (; n < 99; n++)
+            {
+                Console.Write("{0} = {1}\n", n, n.ToString("X"));
+            }
+        }
+    }
+}
