32e6729ca7a84b2f011148d504f4dd7c426bf563
klich404 <klich4010@gmail.com>
2021-11-30 16:25:46 -0500

    task 9, 10 and 11

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        for (int count = 0; count < length; count++)
+            Console.Write("_");
+        Console.WriteLine();
+    }
+}
---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {    
+        int x, y;
+
+        if (length > 0)
+        {
+            for (x = 0; x < length; x++)
+            {
+                for (y = 0; y < x; y++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("\\");
+                Console.WriteLine();
+            }
+        }
+        Console.WriteLine();
+    }
+}
