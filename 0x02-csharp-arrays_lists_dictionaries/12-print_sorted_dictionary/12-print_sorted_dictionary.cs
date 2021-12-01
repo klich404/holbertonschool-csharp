96bbb8838a8d14588ef27c219c2c312a833955fc
klich404 <klich4010@gmail.com>
2021-12-01 18:29:39 -0500

    task 10, 11, 12 and 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> list = new List<string>();
+        foreach (string key in myDict.Keys)
+            list.Add(key);
+        list.Sort();
+        foreach (string key in list)
+            Console.WriteLine("{0}: {1}", key, myDict[key]);
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int bestVal = 0;
+        KeyValuePair<string, int> bestKey = new KeyValuePair<string, int>();
+        foreach (KeyValuePair<string, int> element in myList)
+        {
+            if (element.Value > bestVal)
+            {
+                bestVal = element.Value;
+                bestKey = element;
+            }
+        }
+        return bestKey.Key;
+    }
+}
