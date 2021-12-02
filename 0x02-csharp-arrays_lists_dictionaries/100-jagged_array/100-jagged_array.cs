1592f47729183936754db8e260f0941b4095b9b1
klich404 <klich4010@gmail.com>
2021-12-01 19:07:27 -0500

    task 100

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jagged = new int[][]{
+                new int[] { 0, 1, 2, 3 },
+                new int[] { 0, 1, 2, 3, 4, 5, 6 },
+                new int[] { 0, 1 }
+            };
+            for (int x = 0; x < jagged.Length; x++)
+            {
+                for (int y = 0; y < jagged[x].Length; y++)
+                {
+                    if (y != 0)
+                        Console.Write(" ");
+                    Console.Write(jagged[x][y]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
