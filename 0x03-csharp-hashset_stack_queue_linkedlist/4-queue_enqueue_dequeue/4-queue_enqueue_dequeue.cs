da1bc24b1bae24c6c0c000b85c4f0ad7979ba504
klich404 <klich4010@gmail.com>
2021-12-02 18:43:59 -0500

    task 4 and 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        int count = aQueue.Count;
+        bool isIn = aQueue.Contains(search);
+
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+
+        if (count == 0)
+            Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine("Top item: {0}", aQueue.Peek());
+
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, isIn);
+
+        if (isIn == true)
+            while (aQueue.Dequeue() != search)
+                continue;
+
+        aQueue.Enqueue(newItem);
+        return aQueue;
+    }
+}
