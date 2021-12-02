9bb2e7ac45bca149e4b49f254ad0ef5677a1db74
klich404 <klich4010@gmail.com>
2021-12-02 17:13:04 -0500

    task 0, 1 and readme

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int sum = 0;
+        myList.Sort();
+        for (int count = 0; count < myList.Count; count++)
+        {
+            if (count == 0 || myList[count] != myList[count - 1])
+                sum += myList[count];
+        }
+        return sum;
+    }
+}
---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> newList = new List<int>();
+        for (int x = 0; x < list1.Count; x++)
+        {
+            for (int y = 0; y < list2.Count; y++)
+            {
+                if (list1[x] == list2[y])
+                    newList.Add(list1[x]);
+            }
+        }
+        return newList;
+    }
+}
