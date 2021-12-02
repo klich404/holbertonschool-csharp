35cf0838bb3d8f011dd808ae28168bb97ef76822
klich404 <klich4010@gmail.com>
2021-12-02 18:46:52 -0500

    task 4 correction

--- 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -11,11 +11,11 @@ class MyQueue
         Console.WriteLine("Number of items: {0}", aQueue.Count);
 
         if (count == 0)
-            Console.WriteLine("Stack is empty");
+            Console.WriteLine("Queue is empty");
         else
-            Console.WriteLine("Top item: {0}", aQueue.Peek());
+            Console.WriteLine("First item: {0}", aQueue.Peek());
 
-        Console.WriteLine("Stack contains \"{0}\": {1}", search, isIn);
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, isIn);
 
         if (isIn == true)
             while (aQueue.Dequeue() != search)
