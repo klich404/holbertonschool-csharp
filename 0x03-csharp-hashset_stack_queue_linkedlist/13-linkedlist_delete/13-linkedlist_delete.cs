6e2a637573b82c6d2ae342315ee1cd971792b2f5
klich404 <klich4010@gmail.com>
2021-12-02 21:18:01 -0500

    task 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int ind = 0;
+        LinkedListNode<int> holder = myLList.First;
+
+        for (; ind < index && holder.Next != null; ind++)
+            holder = holder.Next;
+        if (ind == index)
+            myLList.Remove(holder);
+    }
+}
