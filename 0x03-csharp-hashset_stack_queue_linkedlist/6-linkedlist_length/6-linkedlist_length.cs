6fc0a08259f5f33923c847392b58ae600f5a1d7c
klich404 <klich4010@gmail.com>
2021-12-02 19:59:59 -0500

    task 6, 7, 8 and 9

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int count = 0;
+        foreach (int element in myLList)
+            count++;
+        return count;
+    }
+}
