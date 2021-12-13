5ef5d94ca8dbd912bd08904b7c75e9d4a0a4964e
klich404 <klich4010@gmail.com>
2021-12-13 15:39:51 -0500

    task 1

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int result = 0;
+        try {
+            result = a / b;
+        } catch {
+            Console.WriteLine("Cannot divide by zero");
+        } finally {
+            Console.WriteLine("{0} / {1} = {2}", a, b, result);
+        }
+    }
+}
