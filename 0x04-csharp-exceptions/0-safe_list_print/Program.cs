1471b4d92bfb5c4cc1a0d4f5a2f8218c85ee9f2a
klich404 <klich4010@gmail.com>
2021-12-13 15:04:26 -0500

    task 0 and readme

---
+++ 0x04-csharp-exceptions/0-safe_list_print/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int count;
+        for(count = 0; count < n; count++)
+        {
+            try {
+                Console.WriteLine(myList[count]);
+            }
+            catch {
+                return count;
+            }
+        }
+        return count;
+    }
+}
