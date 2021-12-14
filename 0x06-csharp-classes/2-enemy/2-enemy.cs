5ca913235e8c0d9ca55573bb304723c8ae0bd4df
klich404 <klich4010@gmail.com>
2021-12-14 16:16:28 -0500

    task 2

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>A zombie class</summary>
+    public class Zombie
+    {
+        ///<summary>health counter for a zombie obj</summary>
+        public int health;
+
+        ///<summary>A zombie initialization</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        ///<summary>A zombie initialization</summary>
+        public Zombie(int value)
+        {
+            if (value <= 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+    }
+}
