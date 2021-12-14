aa94c6b506ffb7c0835b3c1c64e7194ec3d4f80f
klich404 <klich4010@gmail.com>
2021-12-14 15:49:43 -0500

    task 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Zombie</summary>
+    public class Zombie
+    {
+        int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
+
