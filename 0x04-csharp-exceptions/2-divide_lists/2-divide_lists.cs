924eca6eab0ba48debf5260ab8e8da7ed0a39962
klich404 <klich4010@gmail.com>
2021-12-14 15:30:28 -0500

    task 0 and readme

---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
---
+++ 0x06-csharp-classes/0-enemy/0-enemy.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Zombie</summary>
+    public class Zombie
+    {
+    }
+}
