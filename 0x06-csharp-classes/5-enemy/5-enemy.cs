57140724904e54fc8626f711bde7897fef444e1b
klich404 <klich4010@gmail.com>
2021-12-14 16:46:21 -0500

    task 5

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>A zombie class</summary>
+    public class Zombie
+    {
+        ///<summary>health counter for a zombie obj</summary>
+        public int health;
+        String name = "(No name)";
+
+        ///<summary>A zombie initialization</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        ///<summary>A zombie initialization</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+
+        ///<summary>Return the healt of the zombie obj</summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        ///<summary>set and get the Name property</summary>
+        public string Name
+        {
+            get{return name;}
+            set{name = value;}
+        }
+
+        ///<summary>Print the Zombie obj attribute</summary>
+        public override string ToString()
+        {
+            return "Zombie Name: " + name + " / Total Health: " + health;
+        }
+    }
+}
