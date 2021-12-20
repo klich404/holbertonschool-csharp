6c9837ed54e5d9608b5ba51cbab93226eb3df22e
klich404 <klich4010@gmail.com>
2021-12-20 18:24:53 -0500

    task 5

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary>A Shape class</summary>
+public class Shape
+{
+    ///<summary>A public method who throw a erre</summary>
+    public virtual int Area()
+    {
+        throw new System.NotImplementedException("Area() is not implemented");
+    }
+}
