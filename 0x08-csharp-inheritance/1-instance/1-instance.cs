c3deeed9c7d2188a71c5b1133b38902ffece1e0c
klich404 <klich4010@gmail.com>
2021-12-20 16:58:35 -0500

    task 1

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Obj
+{
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (typeof(Array).IsInstanceOfType(obj) == true)
+            return true;
+        return false;
+    }
+}
