29349442ede3b00e0b45cfcb6e9ec2e912560e0c
klich404 <klich4010@gmail.com>
2021-12-20 16:40:30 -0500

    task 0 and readme

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Obj
+    {
+        ///<summary>returns True if the object is an int, otherwise return False</summary>
+        public static bool IsOfTypeInt(object obj)
+        {
+            if (obj is int)
+                return true;
+            return false;
+        }
+    }
