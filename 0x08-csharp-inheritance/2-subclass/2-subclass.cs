6c9848316080c2ce4543082bb1767ca6f9d43b4b
klich404 <klich4010@gmail.com>
2021-12-20 17:08:59 -0500

    task 2

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Obj
+{
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (baseType.IsSubclassOf(derivedType) == true)
+            return true;
+        return false;
+    }
+}
