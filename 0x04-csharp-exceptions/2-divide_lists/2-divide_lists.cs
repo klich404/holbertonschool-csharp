88c073e12f5f260af6017cfc9af5048fb0706924
klich404 <klich4010@gmail.com>
2022-01-11 14:12:44 -0500

    send last changes

--- 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-class List
-{
-    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
-    {
-        Console.WriteLine("Hello World!");
-    }
-}
---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Obj
+{
+    public static void Print(object myObj)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
