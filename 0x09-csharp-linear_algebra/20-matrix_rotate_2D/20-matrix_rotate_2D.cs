6405d2f3328e70a06c084189012597825dfed6be
klich404 <klich4010@gmail.com>
2022-01-15 18:34:12 -0500

    task 19 and 20

---
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>A VectorMath class</summary>
+class VectorMath
+{
+    ///<summary>Rotates a square 2D matrix by a given angle in radians and returns the resulting matrix</summary>
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        double[,] rotation = new double[2, 2] {{Math.Cos(angle), Math.Sin(angle)}, {-1*Math.Sin(angle), Math.Cos(angle)}};
+        double[,] matrixError = new double[,] {{-1}};
+        double[,] matrixResult = new double[2, 2];
+        double aux;
+
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+            return matrixError;
+
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+            {
+                aux = 0;
+                for (int k = 0; k < 2; k++)
+                    aux += Math.Round(matrix1[i, k] * matrix2[k, j], 2);
+                matrixResult[i ,j] = aux;
+            }
+        return matrixResult;
+    }
+}
