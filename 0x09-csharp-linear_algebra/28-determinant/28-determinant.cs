9d391d2e86abdaf2ba1305f7479a1406922024f0
klich404 <klich4010@gmail.com>
2022-01-18 15:47:32 -0500

    task 29, 30 and 31

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>A VectorMath class</summary>
+class MatrixMath
+{
+    ///<summary>Calculates the determinant of a matrix</summary>
+    public static double Determinant(double[,] matrix)
+    {
+        double[,] matrixError = new double[,] {{-1}};
+        double result = 0;
+
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+            result = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+        else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
+            {
+                result = Math.Round(
+                    (matrix[0, 0] * matrix[1, 1] * matrix[2, 2]) +
+                    (matrix[1, 0] * matrix[2, 1] * matrix[0, 2]) +
+                    (matrix[2, 0] * matrix[0, 1] * matrix[1, 2]) -
+                    (matrix[0, 0] * matrix[2, 1] * matrix[1, 2]) -
+                    (matrix[2, 0] * matrix[1, 1] * matrix[0, 2]) -
+                    (matrix[1, 0] * matrix[0, 1] * matrix[2, 2]), 2);
+            }
+        else
+            return matrixError;
+            
+        return matrixResult;
+    }
+}
---
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>A VectorMath class</summary>
+class MatrixMath
+{
+    ///<summary>Calculates the inverse of a 2D matrix and returns the resulting matrix</summary>
+    public static double[,] Inverse2D(double[,] matrix)
+    {
+        double[,] matrixError = new double[,] {{-1}};
+        double aux;
+
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+        {
+            aux = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+            
+            if (aux == 0)
+                return matrixError;
+
+            double[,] matrixResult = new double[,] {
+                {(1 / aux) * matrix[1, 1], (1 / aux) * -matrix[0, 1]},
+                {(1 / aux) * -matrix[1, 0], (1 / aux) * matrix[0, 0]},
+            };
+            return matrixResult;
+        }
+        return matrixError;
+    }
+}
