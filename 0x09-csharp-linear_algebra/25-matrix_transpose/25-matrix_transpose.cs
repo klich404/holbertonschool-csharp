6e0a7f7edd8cfb17e77ba3490466aac3cc35cfbf
klich404 <klich4010@gmail.com>
2022-01-18 12:27:04 -0500

    task 26 and 27

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>A VectorMath class</summary>
+class MatrixMath
+{
+    ///<summary>Transpose a matrix and return the resulting matrix</summary>
+    public static double[,] Transpose(double[,] matrix)
+    {
+        double[,] matrixError = new double[,] {{-1}};
+        double height = matrix.GetLength(0);
+        double width = matrix.GetLength(1);
+        double[,] matrixResult = new double[width, height];
+
+        for (int i = 0; i < width; i++)
+            for (int j = 0; j < height; j++)
+                matrixResult[i, j] = matrix[j, i];
+        return matrixResult;
+    }
+}
