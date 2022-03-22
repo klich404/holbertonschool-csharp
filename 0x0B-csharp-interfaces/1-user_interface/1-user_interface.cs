fdf754626dd24a7b5cba2fb626d7b2b7554c69d0
klich404 <klich4010@gmail.com>
2022-03-22 17:36:14 -0500

    task 1

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability 
+    {
+        get;
+        set;
+    }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected
+    {
+        get;
+        set;
+    }
+    void Collect();
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability
+    { 
+        get;
+        set; 
+    }
+    public bool isCollected
+    {
+        get;
+        set;
+    }
+    public void Interact()
+    {
+
+    }
+    public void Break()
+    {
+
+    }
+    public void Collect()
+    {
+
+    }
+}
