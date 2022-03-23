38e28533f4e3c39b3857c69c1ae1cf2d312e96f7
klich404 <klich4010@gmail.com>
2022-03-22 19:26:00 -0500

    task 1

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,34 +1,56 @@
 ﻿using System;
 
+/// <summary>
+/// A base class for all of the game objects
+/// </summary>
+abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType().Name}";
+    }
+}
+
+/// <summary>
+/// Defines the properties of an interactive object
+/// </summary>
 interface IInteractive
 {
     void Interact();
 }
 
+/// <summary>
+/// Defines the properties of a breakable object
+/// </summary>
 interface IBreakable
 {
     int durability { get; set; }
-    void Break();
 }
 
+/// <summary>
+/// Defines the properties of a collectable object
+/// </summary>
 interface ICollectable
 {
     bool isCollected { get; set; }
     void Collect();
 }
 
+/// <summary>
+/// A test object for inheriting interfaces
+/// </summary>
 class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
     public int durability { get; set; }
     public bool isCollected { get; set; }
+    
     public void Interact()
     {
 
     }
-    public void Break()
-    {
 
-    }
     public void Collect()
     {
 
