c9526ead3ae6aad4bbbb28fadc6fea5d886b47b8
klich404 <klich4010@gmail.com>
2022-03-23 17:36:32 -0500

    task 3 correction

--- 0x0B-csharp-interfaces/3-decorations/3-decorations.cs
+++ 0x0B-csharp-interfaces/3-decorations/3-decorations.cs
@@ -31,6 +31,22 @@ interface ICollectable
 }
 
 /// <summary> TestObject class that inherits from Base and IIntereactive</summary>
+public class Door : Base, IInteractive
+{
+    ///<summary> new door </summary>
+	public Door(string nam = "Door")
+    {
+        name = nam;
+    }
+
+    ///<summary> Takes place when the object intereacts with something </summary> 
+    public void Interact()
+    {
+        Console.WriteLine("You try to open the {0}. It's locked.", name);
+    }
+}
+
+/// <summary> TestObject class that inherits from Base and IIntereactive</summary>
 public class Decoration : Base, IInteractive, IBreakable
 {
     ///<summary> isQuesItem property </summary>
