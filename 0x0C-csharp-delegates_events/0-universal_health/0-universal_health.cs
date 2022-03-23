b53a4b3d3e62ab40d5fa9597b0e2016a6ce08106
klich404 <klich4010@gmail.com>
2022-03-23 14:06:41 -0500

    task 0 and readme

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+///<summary> Player class </summary>
+class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    ///<summary> COnstructor of player class </summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0f)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+            this.maxHp = maxHp;
+        this.name = name;
+        this.hp = this.maxHp;
+    }
+
+    ///<summary> Prints Player info </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
+    }
+}
