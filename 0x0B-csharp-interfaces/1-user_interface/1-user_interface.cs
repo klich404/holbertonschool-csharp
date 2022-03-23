2845f04b76a698ffc520dff49c7fb0ed3367ef31
klich404 <klich4010@gmail.com>
2022-03-22 19:10:43 -0500

    task 1

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,5 +1,17 @@
 ﻿using System;
 
+public abstract class Base
+{
+    ///<summary> Name of instance </summary>
+    public string name;
+
+    ///<summary> String override </summary>
+    public override string ToString()
+    {
+        return (name + " is a " + this.GetType().ToString());
+    }
+}
+
 interface IInteractive
 {
     void Interact();
