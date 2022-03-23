b782ed64ff23f80dd90693f38512387acc471713
klich404 <klich4010@gmail.com>
2022-03-22 19:32:50 -0500

    task 1

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,38 +1,46 @@
 ﻿using System;
 
-///<summary> Base class </summary>
-public abstract class Base
+/// <summary>
+/// A base class for all of the game objects
+/// </summary>
+abstract class Base
 {
-    ///<summary> Name of instance </summary>
-    public string name;
+    public string name { get; set; }
 
-    ///<summary> String override </summary>
     public override string ToString()
     {
-        return (name + " is a " + this.GetType().ToString());
+        return $"{name} is a {this.GetType().Name}";
     }
 }
 
-/// <summary> Defines the properties of an interactive object </summary>
+/// <summary>
+/// Defines the properties of an interactive object
+/// </summary>
 interface IInteractive
 {
     void Interact();
 }
 
-/// <summary> Defines the properties of a breakable object </summary>
+/// <summary>
+/// Defines the properties of a breakable object
+/// </summary>
 interface IBreakable
 {
     int durability { get; set; }
 }
 
-/// <summary> Defines the properties of a collectable object </summary>
+/// <summary>
+/// Defines the properties of a collectable object
+/// </summary>
 interface ICollectable
 {
     bool isCollected { get; set; }
     void Collect();
 }
 
-/// <summary> A test object for inheriting interfaces </summary>
+/// <summary>
+/// A test object for inheriting interfaces
+/// </summary>
 class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
     public int durability { get; set; }
