0902d931f3a5adf89d8e41cdfd2996e4c32e0749
klich404 <klich4010@gmail.com>
2022-03-22 19:21:45 -0500

    task 1

---
+++ 0x0B-csharp-interfaces/1-user_interface/0-abstract_thinking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+///<summary>Base class.</summary>
+public abstract class Base
+{
+    ///<summary>Name of instance.</summary>
+    public string name;
+
+    ///<summary>String override.</summary>
+    public override string ToString()
+    {
+        return (name + " is a " + this.GetType().ToString());
+    }
+}
--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,44 +1,34 @@
 ﻿using System;
 
-///<summary> Interective interface </summary>
 interface IInteractive
 {
     void Interact();
 }
 
-///<summary> Breakable interface </summary>
 interface IBreakable
 {
     int durability { get; set; }
     void Break();
 }
 
-///<summary> Collectable interface </summary>
 interface ICollectable
 {
     bool isCollected { get; set; }
     void Collect();
 }
 
-///<summary> Class that inherits from Base and all interfaces </summary>
 class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
     public int durability { get; set; }
     public bool isCollected { get; set; }
-
-    ///<summary> Take place when the object interacts with something </summary>
     public void Interact()
     {
 
     }
-
-    ///<summary> Take place when the object brakes </summary>
     public void Break()
     {
 
     }
-
-    ///<summary> Take place when the object collects somenthing </summary>
     public void Collect()
     {
 
