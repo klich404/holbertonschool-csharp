e866e93f2b2acf20481ed9d054e182114e38b304
klich404 <klich4010@gmail.com>
2022-03-28 18:10:53 -0500

    creating new project 0x0D

---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+///<summary> The base clase </summary>
+public class BaseClass
+{
+    public string id { get; set;}
+    public DateTime date_created { get; set; }
+    public DateTime date_updated { get; set; }
+
+    public BaseClass(String id = null, DateTime date_created = default(DateTime), DateTime date_updated = default(DateTime))
+    {
+        if (id == null)
+            this.id = System.Guid.NewGuid().ToString();
+        else
+            this.id = id;
+        if (date_created == default(DateTime))
+            this.date_created = DateTime.Now;
+        else
+            this.date_created = date_created;
+        if (date_updated == default(DateTime))
+           this.date_updated = DateTime.Now;
+        else
+            this.date_updated = date_updated;
+    }
+}
