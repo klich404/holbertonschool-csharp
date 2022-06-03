828f8dbed45110905cb7f594649ee39eea38d00a
klich404 <klich4010@gmail.com>
2022-06-03 17:53:14 -0500

    task 0

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary>Generic class</summary>
+public class Queue<T>
+{
+    ///<summary>returns the Queue’s type</summary>
+    public string CheckType()
+    {
+        return(typeof(T).ToString());
+    }
+}
